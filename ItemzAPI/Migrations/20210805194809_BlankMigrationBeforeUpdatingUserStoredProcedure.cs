093ce30c6e34258bdb5420f5c15e7183ce1fea8f
Dharmesh Shah <dhompia@hotmail.com>
2021-08-06 01:19:47 +0530

    Blank Migration Before Updating User Stored Proc

---
+++ ItemzAPI/Migrations/20210805194809_BlankMigrationBeforeUpdatingUserStoredProcedure.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItemzApp.API.Migrations
+{
+    public partial class BlankMigrationBeforeUpdatingUserStoredProcedure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
