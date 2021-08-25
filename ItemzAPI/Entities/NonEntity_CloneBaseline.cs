d35acbfc6c3c7d1f2aafdfca6b4879f00f4ea391
Dharmesh Shah <dhompia@hotmail.com>
2021-08-26 01:23:25 +0530

    Added detailed comment for NonEntity_CloneBaseline Entity class.

--- ItemzAPI/Entities/NonEntity_CloneBaseline.cs
+++ ItemzAPI/Entities/NonEntity_CloneBaseline.cs
@@ -9,7 +9,11 @@ namespace ItemzApp.API.Entities
     /// NonEntity_CloneBaseline is used to pass in necessary properties to custom User Stored Procedure
     /// via repository service that clones existing baseline to create a new Baseline from it.
     /// 
-    /// This is NOT used for mapping entities to the database via EF Core DBContext
+    /// This is NOT used for mapping entities to the database via EF Core DBContext. 
+    /// 
+    /// We are making sure that we follow the convention of not letting DTO be sent directly to the 
+    /// Database for processing user request. For now, CloneBaselineDTO and NonEntity_CloneBaseline has 
+    /// similar properties and we keep them seperated to make sure that we follow necessary best practices.
     /// </summary>
     public class NonEntity_CloneBaseline
     {
