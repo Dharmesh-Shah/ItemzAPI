ebd408397e81ac9eb76679f41f5f42873bdc8b3d
Dharmesh Shah <dhompia@hotmail.com>
2024-06-03 01:02:10 +0100

    Updated userProcDeleteItemzHierarchyRecordsByItemzId to support ORPHAND Itemz

--- ItemzAPI/DbContexts/UserStoredProcedure/userProcDeleteItemzHierarchyRecordsByItemzId_202406021752.sql
+++ ItemzAPI/DbContexts/UserStoredProcedure/userProcDeleteItemzHierarchyRecordsByItemzId_202406021752.sql
@@ -11,12 +11,7 @@ AS
 
 BEGIN
 BEGIN TRY
-BEGIN TRANSACTION
 
--- FIND ITEMZ HIERARCHY RECORD BY ITEMZ ID 
--- IT SHOULD ONLY BE ONE UNIQUE RECORD IN THE ITEMZ HIERARCHY DB
--- BECAUSE id FIELD IN ItemzHierarchy TABLE IS A PRIMARY KEY...
--- WHEN WE SEARCH BY A SINGLE @ProjectID THEN ONLY ONE RECORD SHOULD BE RETURNED
 
 DECLARE @ItemzAsRoot hierarchyID;
 DECLARE @RecordTypeToBeItemz NVARCHAR(128)
@@ -26,7 +21,6 @@ DECLARE @ItemzHierarchyRowCount int
 -- FIRST set @OUTPUT_Success to 0 (ZERO) so that default is set to value false.
 SET @OUTPUT_Success = 0; -- 1 means true and 0 means false
 
-
 SELECT @ItemzAsRoot = ItemzHierarchyId
 		, @RecordTypeToBeItemz = RecordType
 		,  @HierarchyLevel = ItemzHierarchyId.GetLevel()
@@ -35,17 +29,38 @@ WHERE id = @ItemzId
 
 set @ItemzHierarchyRowCount = @@rowcount
 
-If (@ItemzHierarchyRowCount <> 1) -- If RowCount is not exactly 1 then raise error
+-- IT IS POSSIBLE THAT ITEMZ ID BASED HIERARCHY RECORD WAS NOT PRESENT BECAUSE ITEMZ IS AN ORPHAND ITEMZ.
+-- THIS IS THE REASON WHY WE SIMPLY RETURN BACK FROM THE STORED PROCEDURE WHEN RECORD COUNT IS ZERO
+
+If(@ItemzHierarchyRowCount = 0)
 	BEGIN
-		SET @OUTPUT_Success = 0
-		BEGIN
-		RAISERROR (N'Root element in ItemzHierarchy is not exactly one. Check if the ItemzHierarchy record is found for the given Itemz ID in the Database.', -- Message text.  
-					16, -- Severity.  
-					1 -- State.  
-					)
-		END
-		SET @OUTPUT_Success = 1
+		RETURN
 	END
+END TRY
+BEGIN CATCH
+	RETURN
+END CATCH
+
+BEGIN TRY
+BEGIN TRANSACTION  -- NOW THE ACTUAL TRANSACTION BEGINS WHEN RECORD COUNT IS MORE THEN 0
+
+-- WE ARE NOT GOING TO RAISE ERROR WHEN RowCount is not exactly 1 for ITEMZ because
+-- WE MAY HAVE ORPHANED ITEMZ WHICH IS NOT PART OF ANY PROJECT OR ITEMZ TYPE
+-- AND IT STILL NEEDS TO BE DELETED.
+
+--If (@ItemzHierarchyRowCount <> 1) -- If RowCount is not exactly 1 then raise error
+--	BEGIN
+--		SET @OUTPUT_Success = 0
+--		BEGIN
+--		RAISERROR (N'Root element in ItemzHierarchy is not exactly one. Check if the ItemzHierarchy record is found for the given Itemz ID in the Database.', -- Message text.  
+--					16, -- Severity.  
+--					1 -- State.  
+--					)
+--		END
+--		SET @OUTPUT_Success = 1
+--	END
+
+
 IF (@HierarchyLevel = 0)
 	BEGIN
 		SET @OUTPUT_Success = 0
