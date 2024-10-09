adbb6b38195f68d3bc1036a84d4db97b73824170
Dharmesh Shah <dhompia@hotmail.com>
2024-10-09 14:07:44 +0100

    HttpClient - introduced ItemzChangeHistory

---
+++ ItemzApp.WebUI/ItemzApp.WebUI.Client/Services/ItemzChangeHistory/IItemzChangeHistoryService.cs
@@ -0,0 +1,12 @@
+﻿using ItemzApp.WebUI.Client.SharedModels;
+
+namespace ItemzApp.WebUI.Client.Services.ItemzChangeHistoryService
+{
+    public interface IItemzChangeHistoryService
+	{
+		public Task<ICollection<GetItemzChangeHistoryDTO>> __GET_ItemzChangeHistory_By_GUID_ItemzID__Async(Guid itemzId);
+
+		public Task<int> __DELETE_ItemzChangeHistory_By_GUID_ID__Async(DeleteChangeHistoryDTO body);
+
+	}
+}
