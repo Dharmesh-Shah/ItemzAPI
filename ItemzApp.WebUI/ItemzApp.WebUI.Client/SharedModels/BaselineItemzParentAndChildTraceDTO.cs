aa16ef34a82994c6b79d4dcbbfe9859c5d3593aa
Dharmesh Shah <dhompia@hotmail.com>
2024-10-11 00:43:30 +0100

    HttpClient - Introduced BaselineItemzTraceService

---
+++ ItemzApp.WebUI/ItemzApp.WebUI.Client/SharedModels/BaselineItemzParentAndChildTraceDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemzApp.WebUI.Client.SharedModels
+{
+    public class BaselineItemzParentAndChildTraceDTO
+    {
+        public BaselineItemzParentAndChildTraceDTO()
+        {
+            SingleBaselineItemzAllTrace__DTO singleBaselineItemzAllTrace__DTO = new SingleBaselineItemzAllTrace__DTO();
+            BaselineItemz = singleBaselineItemzAllTrace__DTO;
+        }
+
+        public SingleBaselineItemzAllTrace__DTO? BaselineItemz { get; set; }
+
+    }
+
+    public class SingleBaselineItemzAllTrace__DTO
+    {
+        public Guid ID { get; set; }
+        public List<ParentTraceBaselineItemz__DTO>? ParentBaselineItemz { get; set; }
+        public List<ChildTraceBaselineItemz__DTO>? ChildBaselineItemz { get; set; }
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class ChildTraceBaselineItemz__DTO
+    {
+        public Guid BaselineItemzID { get; set; }
+    }
+
+    public class ParentTraceBaselineItemz__DTO
+    {
+        public Guid BaselineItemzID { get; set; }
+    }
+}
