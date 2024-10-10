0e0162d15330c8ceff0b44a3cf6a636ee42b50a3
Dharmesh Shah <dhompia@hotmail.com>
2024-10-11 00:03:04 +0100

    Minor - removed blank spacing lines

--- ItemzApp.WebUI/ItemzApp.WebUI.Client/Services/BaselineItemzTypes/IBaselineItemzTypesService.cs
+++ ItemzApp.WebUI/ItemzApp.WebUI.Client/Services/BaselineItemzTypes/IBaselineItemzTypesService.cs
@@ -9,14 +9,9 @@ namespace ItemzApp.WebUI.Client.Services.BaselineItemzTypesService
 
 		public Task<ICollection<GetBaselineItemzTypeDTO>> __GET_BaselineItemzTypes_Collection__Async();
 
-
 		public Task<int> __GET_BaselineItemz_Count_By_BaselineItemzType__Async(Guid baselineItemzTypeId);
 
 		public Task<ICollection<GetBaselineItemzDTO>> __GET_BaselineItemzs_By_BaselineItemzType__Async(Guid baselineItemzTypeId, int? pageNumber, int? pageSize, string orderBy);
-
-
-
-
 
 	}
 }
