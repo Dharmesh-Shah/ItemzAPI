d6fcf75b4db1cf0c89d6e836d994c216f8a71569
Dharmesh Shah <dhompia@hotmail.com>
2024-11-26 23:25:54 +0000

    JSIntrop - Resizable MudTreeView
    In ProjectTreeView and BaselineTreeView, we implemented ability to resize treeview by adding draggable DIV around it! This will be very useful when large number of hierarchy structure is introduced for Itemz Tree Nodes.

---
+++ ItemzApp.WebUI/ItemzApp.WebUI/wwwroot/js/site.js
@@ -0,0 +1,68 @@
+﻿
+// wwwroot/js/site.js
+window.initializeResizable = () => {
+    const maxWidth = window.innerWidth * 0.36; // Calculation for MaxWidth to be 36% of remaining space.
+
+    interact('.resizable')
+        .resizable({
+            edges: { left: false, right: true, bottom: false, top: false }, // Enable resizing from left and right edges
+            listeners: {
+                move(event) {
+                    let { x, y } = event.target.dataset;
+
+                    x = (parseFloat(x) || 0) + event.deltaRect.left;
+
+                    Object.assign(event.target.style, {
+                        width: `${event.rect.width}px`,
+                        transform: `translate(${x}px, 0px)`
+                    });
+
+                    Object.assign(event.target.dataset, { x, y });
+                },
+                end(event) {
+                    // Ensure the width is set after resizing ends
+                    let width = event.rect.width;
+                    event.target.style.width = `${width}px`;
+                    event.target.dataset.width = width; // Persist the width as a dataset attribute
+                }
+            },
+            modifiers: [
+                interact.modifiers.restrictEdges({
+                    outer: 'parent',
+                    endOnly: true,
+                }),
+                interact.modifiers.restrictSize({
+                    min: { width: 300 },
+                    max: { width: maxWidth } // Use the dynamically calculated maximum width
+                }),
+            ],
+            inertia: true
+        });
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const maxWidth = window.innerWidth * 0.36;
+    interact('.resizable')
+        .resizable({
+            modifiers: [
+                interact.modifiers.restrictSize({
+                    min: { width: 300 },
+                    max: { width: maxWidth }
+                }),
+            ]
+        });
+});
+
+// Recalculate on window resize
+window.addEventListener('resize', () => {
+    const maxWidth = window.innerWidth * 0.36;
+    interact('.resizable')
+        .resizable({
+            modifiers: [
+                interact.modifiers.restrictSize({
+                    min: { width: 300 },
+                    max: { width: maxWidth }
+                }),
+            ]
+        });
+});
