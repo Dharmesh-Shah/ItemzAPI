fbbedaae44bf765b1422bf840c60c85e1e752ec0
Dharmesh Shah <dhompia@hotmail.com>
2024-11-29 02:10:15 +0000

    Implemented Two Ways Binding between ItemzDetailsComponent and ProjectTreeView
    This way, change in ItemzDetailsComponent where Name has been updated is sent back and hadled in ProjectTreeView for updating it in TreeNodes.

--- ItemzApp.WebUI/ItemzApp.WebUI/Components/EventServices/TreeNodeItemzSelectionService.cs
+++ ItemzApp.WebUI/ItemzApp.WebUI/Components/EventServices/TreeNodeItemzSelectionService.cs
@@ -3,11 +3,19 @@
     public class TreeNodeItemzSelectionService
     {
         public event Action<Guid> OnTreeNodeItemzSelected;
+        public event Action<Guid, string> OnTreeNodeItemzNameUpdated;
 
         public void SelectTreeNodeItemz(Guid recordId)
         {
             OnTreeNodeItemzSelected?.Invoke(recordId);
         }
+
+        public void UpdateTreeNodeItemzName(Guid recordId, string newName)
+        {
+            OnTreeNodeItemzNameUpdated?.Invoke(recordId, newName);
+        }
     }
 }
+
+
 
