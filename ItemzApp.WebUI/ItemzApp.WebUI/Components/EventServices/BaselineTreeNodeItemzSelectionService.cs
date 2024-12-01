6ff7266ea620b184441a09bca5a8abe563092824
Dharmesh Shah <dhompia@hotmail.com>
2024-12-01 02:10:15 +0000

    Implemented EventServices for Include and Exclude of BaselineItemz
    This change will help in making sure that we are able to pass in events to include and exclude single or multiple Itemz into BaselineTreeView to be able to show updates in the client side of the application without going back to the server.
    
    Otherwise a large refresh would be required between client and server just to obtain updated data!

--- ItemzApp.WebUI/ItemzApp.WebUI/Components/EventServices/BaselineTreeNodeItemzSelectionService.cs
+++ ItemzApp.WebUI/ItemzApp.WebUI/Components/EventServices/BaselineTreeNodeItemzSelectionService.cs
@@ -4,8 +4,12 @@
 	{
         public event Action<Guid> OnBaselineTreeNodeItemzSelected;
         public event Action<Guid, string> OnBaselineTreeNodeItemzNameUpdated;
+		public event Action<Guid, bool> OnLoadingOfBaselineItemzTreeViewComponent;
+        public event Action<Guid> OnSingleBaselineItemzIsIncludedChanged;
+        public event Action<Guid> OnExcludeAllChildrenBaselineItemzTreeNodes;
+		public event Action<Guid> OnIncludeAllChildrenBaselineItemzTreeNodes;
 
-        public void SelectBaselineTreeNodeItemz(Guid recordId)
+		public void SelectBaselineTreeNodeItemz(Guid recordId)
         {
             OnBaselineTreeNodeItemzSelected?.Invoke(recordId);
         }
@@ -15,14 +19,35 @@
             OnBaselineTreeNodeItemzNameUpdated?.Invoke(recordId, newName);
         }
 
-        // TODO :: In Baseline, we update it's IsIncluded property
-        // which will have impact on the BaselineItemzTreeView as we will
-        // expect it to change color of the BaselineItemzNode to Red. 
-        // This will impact not only a single BaselineItemzNode but all it's child nodes as well.
-        // So we will have to come-up with another EventService method that can communicate 
-        // change in IsIncluded Property back to BaselineItemzTreeNode to process it accordingly.
+		public void LoadingOfBaselineItemzTreeViewComponent (Guid recordId, bool isIncluded)
+		{
+            OnLoadingOfBaselineItemzTreeViewComponent?.Invoke(recordId, isIncluded);
 
-    }
+        }
+        public void SingleBaselineItemzIsIncludedChanged(Guid recordId)
+        {
+			OnSingleBaselineItemzIsIncludedChanged?.Invoke(recordId);
+		}
+
+        public void ExcludeAllChildrenBaselineItemzTreeNodes(Guid recordId)
+        {
+			OnExcludeAllChildrenBaselineItemzTreeNodes?.Invoke(recordId);
+
+		}
+		public void IncludeAllChildrenBaselineItemzTreeNodes(Guid recordId)
+		{
+			OnIncludeAllChildrenBaselineItemzTreeNodes?.Invoke(recordId);
+
+		}
+
+		// TODO :: In Baseline, we update it's IsIncluded property
+		// which will have impact on the BaselineItemzTreeView as we will
+		// expect it to change color of the BaselineItemzNode to Red. 
+		// This will impact not only a single BaselineItemzNode but all it's child nodes as well.
+		// So we will have to come-up with another EventService method that can communicate 
+		// change in IsIncluded Property back to BaselineItemzTreeNode to process it accordingly.
+
+	}
 }
 
 
